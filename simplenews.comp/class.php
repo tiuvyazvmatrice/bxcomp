6eea61d31ff3f45b34b8df86dac62b47170dfae9
Игорь Амелин <i.amelin@qsoft.ru>
2021-10-05 12:12:32 +0300

    bxcomp заготовка компонента

---
+++ simplenews.comp/class.php
@@ -0,0 +1,29 @@
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+class CSimplenewsComp extends \CBitrixComponent
+{
+    protected $filter = [];
+
+    protected $sort = ['DATE_ACTIVE_FROM' => 'DESC'];
+
+    protected $select = [
+
+    ];
+
+    protected $arNavParams = false;
+
+    public function onPrepareComponentParams($params)
+    {
+
+    }
+
+    public function executeComponent()
+    {
+
+    }
+
+    protected function selectElements()
+    {
+
+    }
+}
