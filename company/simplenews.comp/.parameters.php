7ea29ea1ff9ac6d3ca6fbd73471abdecf52bca45
Игорь Амелин <i.amelin@qsoft.ru>
2021-10-05 12:35:17 +0300

    bxcomp параметры

--- company/simplenews.comp/.parameters.php
+++ company/simplenews.comp/.parameters.php
@@ -1,1 +1,46 @@
-<?php
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+use Bitrix\Main\Localization\Loc;
+use \Bitrix\Main\Loader;
+
+Loc::loadMessages(__FILE__);
+
+if (!Loader::includeModule('iblock'))
+    return;
+
+$arIBlock = [];
+$rsIBlock = \Bitrix\Iblock\IblockTable::getList([
+    'select' => ['ID', 'NAME'],
+    'filter' => ['ACTIVE' => 'Y']
+]);
+
+
+while ($arr = $rsIBlock->Fetch())
+    $arIBlock[$arr['ID']] = '[' . $arr['ID'] . '] ' . $arr['NAME'];
+
+unset($arr, $rsIBlock);
+
+$arComponentParameters = array(
+    "PARAMETERS" => array(
+        "IBLOCK_ID" => array(
+            "PARENT" => "BASE",
+            "NAME" => GetMessage("IBLOCK_ID"),
+            "TYPE" => "LIST",
+            "ADDITIONAL_VALUES" => "Y",
+            "MULTIPLE" => "Y",
+            "SIZE" => 5,
+            "VALUES" => $arIBlock,
+            "REFRESH" => "Y",
+        ),
+        "ELEMENT_COUNT" => array(
+            "PARENT" => "BASE",
+            "NAME" => GetMessage("T_IBLOCK_DESC_LIST_CONT"),
+            "TYPE" => "STRING",
+            "DEFAULT" => 6,
+        ),
+        "CACHE_TIME" => array(
+            "DEFAULT" => 3600
+        )
+    )
+);
--- company/simplenews.comp/class.php
+++ company/simplenews.comp/class.php
@@ -14,12 +14,25 @@ class CSimplenewsComp extends \CBitrixComponent
 
     public function onPrepareComponentParams($params)
     {
+        if ((int)$params['IBLOCK_ID']) {
+            $this->filter['IBLOCK_ID'] = $params['IBLOCK_ID'];
+        }
 
+        $this->arNavParams = array(
+            "nPageSize" => $params["ELEMENT_COUNT"],
+            "bDescPageNumbering" => $params["PAGER_DESC_NUMBERING"],
+            "bShowAll" => $params["PAGER_SHOW_ALL"],
+        );
+        $arNavigation = CDBResult::GetNavParams($this->arNavParams);
+        if($arNavigation["PAGEN"] == 0 && $params["PAGER_DESC_NUMBERING_CACHE_TIME"] > 0)
+            $this->arParams["CACHE_TIME"] = $params["PAGER_DESC_NUMBERING_CACHE_TIME"];
+
+        return $params;
     }
 
     public function executeComponent()
     {
-
+        $this->includeComponentTemplate();
     }
 
     protected function selectElements()
